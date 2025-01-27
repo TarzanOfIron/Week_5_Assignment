249d432ae59b16eca32691a6b91d0a827f0f0b87
Boti <medgyesi.me@icloud.com>
2025-01-27 15:05:18 +0100

    ToDoItems class created and fields, getters, setters and required methods added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         Person person1 = new Person();
@@ -7,5 +9,14 @@ public class Main {
         person1.setLastName("Medgyesi");
         person1.setEmail("asdf");
         System.out.println(person1.getSummary());
+
+        ToDoItem tDI1 = new ToDoItem();
+        tDI1.setDone(false);
+        tDI1.setDeadLine(LocalDate.now().minusDays(1));
+        tDI1.setCreator(person1);
+        if (tDI1.isOverDue()) {
+            System.out.println("true");
+        }
+        System.out.println(tDI1.getSummary());
     }
 }
---
+++ src/main/java/org/example/ToDoItem.java
@@ -0,0 +1,96 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class ToDoItem {
+
+    // Fields
+
+    private int id;
+
+    private String title;
+
+    private String taskDescription;
+
+    private LocalDate deadLine;
+
+    private boolean done;
+
+    private Person creator;
+
+
+    // Getters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public LocalDate getDeadLine() {
+        return deadLine;
+    }
+
+    public Person getCreator() {
+        return creator;
+    }
+
+
+    // Setters
+
+
+    public void setTitle(String title) {
+        if (title.trim().isEmpty()) {
+            throw new IllegalArgumentException("Title missing");
+        }
+        this.title = title;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        if (taskDescription.trim().isEmpty()) {
+            throw new IllegalArgumentException("Task description missing");
+        }
+        this.taskDescription = taskDescription;
+    }
+
+    public void setDeadLine(LocalDate deadLine) {
+        if (deadLine == null) {
+            throw new IllegalArgumentException("Task deadline missing");
+        }
+        this.deadLine = deadLine;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public void setCreator(Person creator) {
+        this.creator = creator;
+    }
+
+
+    // Methods
+
+    public boolean isDone () {
+        return done;
+    }
+
+    public boolean isOverDue() {
+        return getDeadLine().isBefore(LocalDate.now());
+    }
+
+    public String getSummary() {
+        return  ("ID: " + id +
+                ", Title: " + title +
+                ", Task Description: " + taskDescription +
+                ", Deadline: " + deadLine +
+                ", Done: " + done +
+                ", Creator: " + creator.getId());
+    }
+}
